eeb9b2b122241ee1a933ef766e7ddb48e54f7ab6
Judismar Arpini Junior <jj.arpini@gmail.com>
2021-03-26 10:15:44 -0300

    Updating

--- average-time-simulations/cuckoo_hashing_perfect_rehash.py
+++ average-time-simulations/cuckoo_hashing_perfect_rehash.py
@@ -13,6 +13,7 @@ class CuckooHashingPerfectRehash:
 		self.T = [None]*self.d
 		self.h = [None]*self.d
 		self.hPerfect = [None]*self.d
+		self.numRehashes = 0
 		self.numberKeys = [0]*self.d #Number of keys in each table.
 		for i in range(self.d):
 			self.h[i] = HashFunction(self.m, 10000019)
@@ -26,9 +27,9 @@ class CuckooHashingPerfectRehash:
 				return True
 		return False
 
-	def insert(self, x):
+	def insert(self, x, rehashAll = False):
 		if self.lookup(x):
-			return
+			return False
 		moves = 0
 		while moves < self.maxmoves:
 			k = random.choice(list(range(self.d)))
@@ -39,36 +40,77 @@ class CuckooHashingPerfectRehash:
 				self.numberKeys[k] += 1
 				if self.numberKeys[k] == self.m:
 					self.emptySlotedTables.remove(k)
-				return
+				return True
 			moves += 1
-		self.perfectRehash(x)
+		self.perfectRehash(x, rehashAll)
+		return True
 
-	def perfectRehash(self, key):
-		print('oi')
-		k = random.choice(self.emptySlotedTables)
-		self.h[k] = HashFunction(self.m, 10000019) #Change the k-th function to generate new underlying augmenting paths.
-		aux = [key]
-		for x in self.T[k]:
-			if x != None:
-				aux.append(x)
-		h = ModifiedPerfectHashFunction(2, 10000019)
-		sucessful = h.mapping(10*len(aux)^2, aux, self.m - len(aux)) #10x^2 iterations, x = len(aux); x is at most m.
-		if sucessful:
-			self.T[k] = [None]*self.m
-			self.hPerfect[k] = h
-		else:
-			print('no!')
-			h = DeterministicPerfectHashFunction(aux, self.u)
-			if h.q >= self.m:
-				self.T[k] = [None]*h.q
-			else:
+	def removeRandom(self):
+		k = random.randint(0, self.d-1)
+		l = random.randint(0, self.m-1)
+		while self.T[k][l] == None:
+			k = random.randint(0, self.d-1)
+			l = random.randint(0, self.m-1)
+		self.T[k][l] = None
+		self.numberKeys[k] -= 1
+		if self.numberKeys[k] == self.m - 1:
+			self.emptySlotedTables.append(k)
+
+	def perfectRehash(self, key, rehashAll):
+		self.numRehashes += 1
+		if not rehashAll: #Perfect rehash on a single table
+			k = random.choice(self.emptySlotedTables)
+			self.h[k] = HashFunction(self.m, 10000019) #Change the k-th function to generate new underlying augmenting paths.
+			aux = [key]
+			for x in self.T[k]:
+				if x != None:
+					aux.append(x)
+			h = ModifiedPerfectHashFunction(2, 10000019)
+			sucessful = h.mapping(10*len(aux)^2, aux, self.m - len(aux)) #10x^2 iterations, x = len(aux); x is at most m.
+			if sucessful:
 				self.T[k] = [None]*self.m
-			self.hPerfect[k] = h
-		for x in aux:
-			self.T[k][self.hPerfect[k](x)] = x
-		self.numberKeys[k] += 1
-		if self.numberKeys[k] == self.m:
-			self.emptySlotedTables.remove(k)
+				self.hPerfect[k] = h
+			else:
+				h = DeterministicPerfectHashFunction(aux, self.u)
+				if h.q >= self.m:
+					self.T[k] = [None]*h.q
+				else:
+					self.T[k] = [None]*self.m
+				self.hPerfect[k] = h
+			for x in aux:
+				self.T[k][self.hPerfect[k](x)] = x
+			self.numberKeys[k] += 1
+			if self.numberKeys[k] == self.m:
+				self.emptySlotedTables.remove(k)
+		else: #Rehash all tables, used in experiments
+			keyStored = False
+			for k in range(self.d):
+				self.h[k] = HashFunction(self.m, 10000019) #Change all functions to generate new underlying augmenting paths.
+				if self.numberKeys[k] > 0 or not keyStored:
+					aux = []
+					if not keyStored and self.numberKeys[k] < self.m:
+						self.numberKeys[k] += 1
+						if self.numberKeys[k] == self.m:
+							self.emptySlotedTables.remove(k)
+						aux.append(key)
+						keyStored = True
+					for x in self.T[k]:
+						if x != None:
+							aux.append(x)
+					h = ModifiedPerfectHashFunction(2, 10000019)
+					sucessful = h.mapping(10*len(aux)^2, aux, self.m - len(aux)) #10x^2 iterations, x = len(aux); x is at most m.
+					if sucessful:
+						self.T[k] = [None]*self.m
+						self.hPerfect[k] = h
+					else:
+						h = DeterministicPerfectHashFunction(aux, self.u)
+						if h.q >= self.m:
+							self.T[k] = [None]*h.q
+						else:
+							self.T[k] = [None]*self.m
+						self.hPerfect[k] = h
+					for x in aux:
+						self.T[k][self.hPerfect[k](x)] = x
 
 	def printf(self):
 		for i in range(self.d):
---
+++ average-time-simulations/rehash_simulations.py
@@ -0,0 +1,50 @@
+import math
+import random
+import sys
+import time
+from cuckoo_hashing_perfect_rehash import CuckooHashingPerfectRehash
+
+random.seed()
+N = 200
+
+def sim(outputfile, rehashAll):
+	n = 500
+	output = "x = 500:500:25000\ny = ["
+	U = list(range(1, 10**7))
+	for _ in range(50):
+		S = random.sample(U, n)
+		chpr = CuckooHashingPerfectRehash(n, 0.5, 3*math.log(n, 2), 10**7)
+		for key in S:
+			chpr.insert(key, True) #Here, we want all functions to change if a rehash is necessary, before the experiment begins.
+		l = []
+		for __ in range(N):
+			chpr.numRehashes = 0
+			cont = 0
+			while chpr.numRehashes < 2:
+				if chpr.numRehashes >= 1:
+					cont += 1
+				chpr.removeRandom()
+				newInsertion = False
+				while not newInsertion:
+					x = random.randint(1, 10**7)
+					newInsertion = chpr.insert(x, rehashAll)
+			l.append(cont)
+		output += str(mean(l)) + " "
+		print(n)
+		n += 500
+		f = open(outputfile, "w")
+		f.write(output)
+		f.close()
+	output += "]\nplot(x, y)"
+	f = open(outputfile, "w")
+	f.write(output)
+	f.close()
+
+def mean(numlist):
+	sumvar = 0
+	for num in numlist:
+		sumvar += num
+	return float(sumvar)/float(len(numlist))
+
+sim("/home/judismar/Área de Trabalho/out", False)
+sim("/home/judismar/Área de Trabalho/out-2", True)
